0e457cc026b82d247082df381dc09db7bfe83198
YUKIHIME-AKASH\akash <akash1999@hotmail.nl>
2024-01-10 19:23:58 +0100

    DarkMode switch to keep me sane

--- egyptian-hieroglyph-trainer-app/src/App.tsx
+++ egyptian-hieroglyph-trainer-app/src/App.tsx
@@ -3,6 +3,15 @@ import HieroglyphSetProvider from "./services/HieroglyphSetProvider"
 
 function App() {
 
+    const switchTheme = () => {
+        if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
+            document.documentElement.setAttribute('data-bs-theme','light')
+        }
+        else {
+            document.documentElement.setAttribute('data-bs-theme','dark')
+        }
+    }
+
     const { sign, transliteration } = HieroglyphSetProvider.getRandomUniliteralPair();
 
     const answers: string[] = HieroglyphSetProvider.getFixedAmountOfRandomTransliterationsExcludingSpecifiedCharacters(3, [transliteration]);
@@ -12,6 +21,7 @@ function App() {
 
     return (
         <>
+            <button className="btn btn-primary" onClick={switchTheme}>Switch theme</button>
             <div className="container text-center">
                 <Quiz question={sign} answers={answers} correctAnswer={transliteration}  />
             </div>
